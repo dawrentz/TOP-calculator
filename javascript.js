2d3b88788fb728996b89537295bc1f065cbbac17
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-04-18 12:59:16 -0500

    Complete initial html and css. Move on to js
    Declare value buttons in js

---
+++ javascript.js
@@ -0,0 +1,49 @@
+// authenticate as admin to divide by zero
+
+//assign buttons
+// const acBtn = document.querySelector("#acBtn");
+// const powerBtn = document.querySelector("#powerBtn");
+// const oneBtn = document.querySelector("#oneBtn");
+// const twoBtn = document.querySelector("#twoBtn");
+// const threeBtn = document.querySelector("#threeBtn");
+// const fourBtn = document.querySelector("#fourBtn");
+// const fiveBtn = document.querySelector("#fiveBtn");
+// const sixBtn = document.querySelector("#sixBtn");
+// const sevenBtn = document.querySelector("#sevenBtn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+// const Btn = document.querySelector("#Btn");
+
+// collect value buttons
+const allBtns = document.querySelectorAll(".valueBtn");
+// console.log(allBtns);
+
+// assign event listener to value buttons
+allBtns.forEach(button => {
+    button.addEventListener("click", e => console.log(button.textContent))
+});
+
+
+
+
+
+
+
+
+
+
+// const test = document.querySelector("#powerBtn");
+// test.addEventListener("click", e => console.log("test"));
+// console.log(test);
+
+
+
+
+// functions
