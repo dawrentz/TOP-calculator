382f4e2255d60a959f18a28c9876598c575737cf
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-04-23 16:06:45 -0500

    Add keyboard support. Fix minor bug.
    Reworked click functions to accept generic values from click or keydown.
    Bug fix where "Enter" repeats last click.

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ let currentDisplayVal = display.textContent;
 let firstNum;
 let secondNum;
 let operator;
-let firstClick = true;
+let overwriteable = true;
 let powerOn = true;
 let prevSecondNum;
 let prevOperator;
@@ -22,6 +22,7 @@ function updateDisplay(value) {
     display.textContent = value;
 }
 
+//===========================================================================//
 //round if too long for display
 function round(num) {
     //bug fix for when num is in sci notation
@@ -45,6 +46,9 @@ function round(num) {
         if (countChar(wholeNum) > 9) {
             return num.toExponential(2);
         }
+        else if (decNum > 0.0000001 && wholeNum != 0 && decNum != 0) {
+            return Math.round(num);
+        }
         //middle numbers need all whole numbers and rounded decimals to a variable length
         else if (decNum > 0.0000001) {
             let roundTo = 9 - countChar(wholeNum) - 1; //gets max decimal length. -1 for decimal point itself
@@ -61,15 +65,21 @@ function round(num) {
     else return num;
 }
 
+//===========================================================================//
 function equals(num1, num2, op) {
     let temp;
+    //bug fix from keyboard implementation
+    if (op == "÷") { op = "/";}
+    if (op == "x") { op = "*";}
+
+
     //operators and special case
     if (op == "+") temp = +num1 + +num2;
     if (op == "-") temp = +num1 - +num2;
-    if (op == "x") temp = +num1 * +num2;
-    if (op == "÷" && secondNum == 0) {
+    if (op == "*") temp = +num1 * +num2;
+    if (op == "/" && secondNum == 0) {
         temp = "Go to jail";
-    } else if (op == "÷") temp = +num1 / +num2;
+    } else if (op == "/") temp = +num1 / +num2;
 
     //store calculation in memory just in case needed later
     prevSecondNum = secondNum;
@@ -84,7 +94,7 @@ function equals(num1, num2, op) {
     firstNum = display.textContent;
     secondNum = undefined;
     operator = undefined;
-    firstClick = true;
+    overwriteable = true;
 
     //reset all values if special case
     if (temp == "Go to jail") {        
@@ -98,56 +108,69 @@ function equals(num1, num2, op) {
 //eventListeners
 //===========================================================================//
 
-//capture current display's value on every click
-document.querySelector("#keys").addEventListener("click", e => {
-    //bug fix for special case
-    if (display.textContent == "Go to jail") {
-        currentDisplayVal = undefined;
-    } 
-    //track display internally
-    else {currentDisplayVal = display.textContent;}
+//capture current display's value on every event
+function updater() {
+        //bug fix for special case
+        if (display.textContent == "Go to jail") {
+            currentDisplayVal = undefined;
+        } 
+        //track display internally
+        else {currentDisplayVal = display.textContent;}
+    
+        // tester: returns internal values on each event
+        console.log("new================");
+        console.log("currentDisplayVal: " + currentDisplayVal);
+        console.log("firstNum: " + firstNum);
+        console.log("secondNum: " + secondNum);
+        console.log("operator: " + operator);
+        console.log("prevSecondNum: " + prevSecondNum);
+        console.log("prevOperator: " + prevOperator);
+        console.log("typeof currentDisplayVal: " + typeof currentDisplayVal);
+}
 
-    //tester: returns internal values on each click
-    console.log("new================");
-    console.log("currentDisplayVal: " + currentDisplayVal);
-    console.log("firstNum: " + firstNum);
-    console.log("secondNum: " + secondNum);
-    console.log("operator: " + operator);
-    console.log("prevSecondNum: " + prevSecondNum);
-    console.log("prevOperator: " + prevOperator);
-    console.log("typeof currentDisplayVal: " + typeof currentDisplayVal);
-});
+document.querySelector("#keys").addEventListener("click", updater);
+
 
 //===========================================================================//
-//change display with button click
 const allValueBtns = document.querySelectorAll(".valueBtn");
 
-allValueBtns.forEach(valueBtn => {    
-    valueBtn.addEventListener("click", function() {
-        //after overwrite
-        if (!firstClick) {
-            if (currentDisplayVal == "0") {
-                updateDisplay(valueBtn.textContent);
-            } else if (currentDisplayVal.length < 9 && currentDisplayVal[0] != "-") { 
-                updateDisplay(currentDisplayVal + valueBtn.textContent);
-            } else if (currentDisplayVal.length < 10 && currentDisplayVal[0] == "-") { 
-                updateDisplay(currentDisplayVal + valueBtn.textContent);
-            }
-        }
+//change display with button event
+function valueToDisplay(e) {
+    let value;
+    //determine type of event and extract value accordingly
+    if (e.type == "click") {
+        value =  e.target.textContent;
+    } else if (e.type == "keydown") {
+        value =  e.key;
+    }
 
-        //overwrite (only needs to overwrite in certain scenarios)
-        if (firstClick) {
-            updateDisplay(valueBtn.textContent);
-            firstClick = false;
+    //after an overwrite. Max lengths diff for pos/neg
+    if (!overwriteable) {
+        if (currentDisplayVal == "0") {
+            updateDisplay(value);
+        } else if (currentDisplayVal.length < 9 && currentDisplayVal[0] != "-") { 
+            updateDisplay(currentDisplayVal + value); //string concat
+        } else if (currentDisplayVal.length < 10 && currentDisplayVal[0] == "-") { 
+            updateDisplay(currentDisplayVal + value); //string concat
         }
-    });
+    }
+
+    //only needs to overwrite in certain scenarios
+    if (overwriteable) {
+        updateDisplay(value);
+        overwriteable = false;
+    }
+}
+
+allValueBtns.forEach(valueBtn => {    
+    valueBtn.addEventListener("click", valueToDisplay);
 });
 
 //===========================================================================//
 //add backspace mechanic
 const backSpaceBtn = document.querySelector("#backSpaceBtn");
 
-backSpaceBtn.addEventListener("click", function() {
+function backspacer() {
     //lots of unrelated scenarios to respond to    
     //scenario 0: special case, positive single digit interger, sci notation, or zero
     if (display.textContent == "Go to jail" || 
@@ -168,14 +191,16 @@ backSpaceBtn.addEventListener("click", function() {
         updateDisplay(display.textContent.slice(0, -1));
     }
 
-    firstClick = false; //remove overwrite ability after user commits to change
-});
+    overwriteable = false; //remove overwrite ability after user commits to change
+}
+
+backSpaceBtn.addEventListener("click", backspacer);
 
 //===========================================================================//
 //add negation mechanic
 const negativeBtn = document.querySelector("#negativeBtn");
 
-negativeBtn.addEventListener("click", function() {    
+function negator() {    
     //scenario 0: special case or zero. Avoids "-0" in display
     if (display.textContent == "Go to jail" || currentDisplayVal == 0) {
         return; // do nothing
@@ -187,14 +212,16 @@ negativeBtn.addEventListener("click", function() {
     //scenario 2: number is negative. Chop off first char
     else (updateDisplay(display.textContent.slice(1)))
     
-    firstClick = false; //remove overwrite ability after user commits to change
-});
+    overwriteable = false; //remove overwrite ability after user commits to change
+}
+
+negativeBtn.addEventListener("click", negator);
 
 //===========================================================================//
 //add decimal ability
 const decimalBtn = document.querySelector("#decimalBtn");
 
-decimalBtn.addEventListener("click", function() {    
+function addDecimal() {    
     if (currentDisplayVal.includes(".")) {
         return; // do nothing
     } 
@@ -203,46 +230,57 @@ decimalBtn.addEventListener("click", function() {
         updateDisplay(currentDisplayVal + ".");
     }
 
-    firstClick = false; //remove overwrite ability after user commits to change
-});
+    overwriteable = false; //remove overwrite ability after user commits to change
+}
+
+decimalBtn.addEventListener("click", addDecimal);
 
 //===========================================================================//
 //add operators
 const allOperatorBtns = document.querySelectorAll("#operators button");
 
+function operatorChoose(e) {
+    let value;
+    //determine type of event and extract value accordingly
+    if (e.type == "click") {
+        value =  e.target.textContent;
+    } else if (e.type == "keydown") {
+        value =  e.key;
+    }
+    
+    //scenario 0: special case
+    if (display.textContent == "Go to jail") {
+        return; // do nothing
+    } 
+    //scenario 1: fresh start. No firstNum yet
+    else if (operator == undefined) {
+        firstNum = currentDisplayVal;
+        operator = value;
+        overwriteable = true; //allow overwrite on next event
+    } 
+    //scenario 2: user needs to change selected operator mid use. firstNum is set and no overwrite must have been done. 
+    else if (operator !== undefined && overwriteable) {
+        operator = value;
+        //overwriteable is still true and unchanged here
+    } 
+    //scenario 3: user wants to use the selected operator on the answer of the first equation. Runs equals and sets up scenario 2.
+    else if (operator !== undefined && !overwriteable) {
+        secondNum = currentDisplayVal;
+        equals(firstNum, secondNum, operator);
+        operator = value;
+        overwriteable = true; //allow overwrite on next event
+    } 
+}
+
 allOperatorBtns.forEach(operatorBtn => {    
-    //extracts textContent for equals() to look at
-    operatorBtn.addEventListener("click", function() {
-        //scenario 0: special case
-        if (display.textContent == "Go to jail") {
-            return; // do nothing
-        } 
-        //scenario 1: fresh start. No firstNum yet
-        else if (operator == undefined) {
-            firstNum = currentDisplayVal;
-            operator = operatorBtn.textContent;
-            firstClick = true; //allow overwrite on next click
-        } 
-        //scenario 2: user needs to change selected operator mid use. firstNum is set and no overwrite must have been done. 
-        else if (operator !== undefined && firstClick) {
-            operator = operatorBtn.textContent;
-            //firstClick is still true and unchanged here
-        } 
-        //scenario 3: user wants to use the selected operator on the answer of the first equation. Runs equals and sets up scenario 2.
-        else if (operator !== undefined && !firstClick) {
-            secondNum = currentDisplayVal;
-            equals(firstNum, secondNum, operator);
-            operator = operatorBtn.textContent;
-            firstClick = true; //allow overwrite on next click
-        } 
-    });
+    operatorBtn.addEventListener("click", operatorChoose);
 })
 
 //===========================================================================//
 //create execute (equals) mechanic
 const equalsBtn = document.querySelector("#equalsBtn");
 
-equalsBtn.addEventListener("click", function() {
+function equator() {
     //scenario 0: bug fix
     if (firstNum == undefined) {
         return; // do nothing
@@ -251,7 +289,7 @@ equalsBtn.addEventListener("click", function() {
     //presence of preSecondNum and preOperator flags that user wants scenario 1
     else if (secondNum == undefined && operator == undefined &&
         prevSecondNum !== undefined && prevOperator !== undefined &&
-        firstClick) {
+        overwriteable) {
             secondNum = prevSecondNum;
             operator = prevOperator;
             equals(firstNum, secondNum, operator);
@@ -261,7 +299,9 @@ equalsBtn.addEventListener("click", function() {
           secondNum = currentDisplayVal;
           equals(firstNum, secondNum, operator);
         }
-});
+}
+
+equalsBtn.addEventListener("click", equator);
 
 //===========================================================================//
 //add all clear
@@ -273,7 +313,7 @@ function allClear() {
     firstNum = undefined;
     secondNum = undefined;
     operator = undefined;
-    firstClick = true;
+    overwriteable = true;
     prevSecondNum = undefined;
     prevOperator = undefined;
 }
@@ -292,8 +332,7 @@ powerBtn.addEventListener("click", function() {
     if (powerOn) {
         powerOn = false;
         noInput();
-
-        //all clicks = nothing
+        //all events = nothing
         document.querySelector("#keys").addEventListener("click", noInput);
 
     } else if (!powerOn) {
@@ -303,12 +342,32 @@ powerBtn.addEventListener("click", function() {
     }
 });
 
-
 //===========================================================================//
 //keyboard implementation
 
-//put functions seperate then assign in eventlisteners, may help with keyboard
-document.addEventListener("keydown", (event) => {
-    console.log(event.key);
+document.addEventListener("keydown", event => {
+    if (event.key >= 0 && event.key <= 9) {valueToDisplay(event)};
+    if (event.key == "Backspace") {backspacer()};
+    // if (event.key == "-") {negator()}; //interferes with subtraction
+    if (event.key == ".") {addDecimal()};
 
+    if (event.key == "+" || event.key == "-" ||
+        event.key == "x" || event.key == "*" || 
+        event.key == "/") {
+            operatorChoose(event)
+        };
+
+    if (event.key == "Enter") {equator();}
+    if (event.key == "Delete") {allClear();}
+
+    updater();
+});
+
+//bug fix: "Enter" repeats last click command
+let allButtons = document.querySelectorAll("button");
+
+allButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        this.blur();
+    });
 });
